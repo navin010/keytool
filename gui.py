f26aaa990d0bbbb7f5bf2bd3f38e7684250cbf43
Navin Lubhaya <31217304+navinRBI@users.noreply.github.com>
2018-11-28 19:58:44 +0000

    working password line edits

--- gui.py
+++ gui.py
@@ -35,17 +35,27 @@ class Window(QtWidgets.QWidget):
         self.init_ui()
 
     def init_ui(self):
+        #b1
         self.b1 = QtWidgets.QPushButton('Open left keystore')
-        self.l1 = QtWidgets.QLabel('Select left keystore')
+        self.l1 = QtWidgets.QLabel()
+        self.p1 = QtWidgets.QLineEdit('password')
+        #b2
         self.b2 = QtWidgets.QPushButton('Open right keystore')
-        self.l2 = QtWidgets.QLabel('Select right keystore')
+        self.l2 = QtWidgets.QLabel()
+        self.p2 = QtWidgets.QLineEdit('password')
+        #b3
         self.b3 = QtWidgets.QPushButton('Run')
 
         v_box = QtWidgets.QVBoxLayout()
+        #b1
         v_box.addWidget(self.b1)
+        v_box.addWidget(self.p1)
         v_box.addWidget(self.l1)
+        #b2
         v_box.addWidget(self.b2)
+        v_box.addWidget(self.p2)
         v_box.addWidget(self.l2)
+        #b3
         v_box.addWidget(self.b3)
 
         self.setLayout(v_box)
@@ -63,7 +73,7 @@ class Window(QtWidgets.QWidget):
 
         if sender.text() == 'Open left keystore':
             filename = QtWidgets.QFileDialog.getOpenFileNames(self, "Open a file", "C://")  # self, window name, directory start path, .jks file extension filter e.g. "JKS File (*.jks)"
-            self.l1.setText(str(filename[0]))  # grab the first csv value, dont need the All files()* bit
+            self.l1.setText(str(filename[0]))  # grab the first csv value, don't need the All files()* bit
             global leftLocation
             leftLocation = str(filename[0])
 
@@ -75,8 +85,9 @@ class Window(QtWidgets.QWidget):
 
     def storeFileDialog(self):
         print(leftLocation)
+        print(self.p1.text())
         print(rightLocation)
-
+        print(self.p2.text())
 
 
 app = QtWidgets.QApplication(sys.argv)
