bf2b037bee6fac8f3f00e4520ca933a833cebad0
Navin Lubhaya <31217304+navinRBI@users.noreply.github.com>
2018-11-29 22:38:42 +0000

    notes from testing

--- notes.py
+++ notes.py
@@ -20,3 +20,32 @@ print('op = ' + test)
 test = 'mkdir ' + '"' + ks1_location.rsplit("\\",1)[0] + "\\" + 'keytoolcerts' + '"'
 print('op = ' + test)
 
+
+ks1_keytoolscerts = ks1_location.rsplit("/", 1)[0] + "/" + 'keytoolcerts'  # create dir for certs
+os.system('mkdir ' + '"' + ks1_keytoolscerts + '"')  # make temporary directory for exported certs
+csv_reader = csv.reader(ds)  # read as csv
+next(csv_reader)  # ignore header line
+for index, line in enumerate(csv_reader):  # loop through lines, enumeration required for indexing
+    alias = line[0]  # left csv value
+    export_cert_cmd = 'keytool -export -alias ' + '"' + alias + '"' + ' -file ' + '"' + ks1_keytoolscerts + "/" + alias + '.cer' + '"' + ' -keystore ' + '"' + ks1_location + '"' + ' -storepass ' + ks1_pass
+    os.system(export_cert_cmd)
+    alias_checked = keytool.check_alias_unique(alias, ds2_nd)  # check using right/second non dropped data store
+    import_cert_cmd = 'keytool -importcert -file ' + '"' + ks1_keytoolscerts + "/" + alias + '.cer' + '"' + ' -keystore ' + '"' + ks2_location + '"' + ' -storepass ' + ks2_pass + ' -alias ' + '"' + str(
+        alias_checked) + '"'
+    os.system(import_cert_cmd)
+os.system('rd /s /q ' + '"' + ks1_keytoolscerts + '"')  # remove temp dir and files(/s) quietly(/q)
+
+
+
+terminal_output_string = subprocess.check_output(terminal_output, shell=True)  # read input from cmd
+
+
+def sendCommand(self):
+    sender = self.sender()  # find button sending
+    if sender.text() == 'Yes':
+        # return os.system('yes')
+        subprocess.call('yes', shell=True)
+    if sender.text() == 'No':
+        command = 'keytool -importcert -file "C:/Users/lubhayan/Documents/Clients/Internal/KeystoreConsolidation/NewGlobalKS/aws/agrewcappo055v.rbi.web.ds/keytoolcerts/nexus_testing_puto.cer" -keystore "C:/Users/lubhayan/Documents/Clients/Internal/KeystoreConsolidation/NewGlobalKS/aws/agrewcappo055v.rbi.web.ds/el_puto_2.jks" -storepass 3point142 -alias "nexus_testing_puto"'
+        print(command)
+        subprocess.call(command, shell=True)
